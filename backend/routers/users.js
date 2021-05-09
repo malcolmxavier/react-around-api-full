ac65dd4abf5cb9007d637cf4990734f94fc38c82
Malcolm Xavier <malcolm.x.evans@gmail.com>
2021-05-08 22:31:59 -0400

    fix user router

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -6,27 +6,27 @@ const {
   getUsers, getOneUser, updateUser, updateUserAvatar,
 } = require('../controllers/userController');
 
-userRouter.get('/users', getUsers);
+router.get('/users', getUsers);
 
-userRouter.get('/users/me', celebrate({
+router.get('/users/me', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
   }),
 }), getCurrentUser);
 
-userRouter.get('/users/:id', celebrate({
+router.get('/users/:id', celebrate({
   params: Joi.object().keys({
     _id: Joi.string().hex().length(24).required(),
   }),
 }), getUser);
 
-userRouter.patch('/users/me/avatar', celebrate({
+router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().uri(),
   }),
 }), updateAvatar);
 
-userRouter.patch('/users/me', celebrate({
+router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),

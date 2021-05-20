52066cf1e71f7093b22c3adafc1a8b4aa6a6e12c
Malcolm Xavier <malcolm.x.evans@gmail.com>
2021-05-19 20:22:03 -0400

    remove console logs

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,6 @@ const UnauthorizedError = require('../middlewares/errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  console.log(req.headers);
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError('Authorization Required');
@@ -11,11 +10,9 @@ module.exports = (req, res, next) => {
 
   const token = authorization.replace('Bearer ', '');
   let payload;
-  console.log('payload: ' + payload);
 
   try {
     payload = jwt.verify(token);
-    console.log('payload: ' + payload);
   } catch (err) {
     throw new UnauthorizedError('Authorization Required');
   }

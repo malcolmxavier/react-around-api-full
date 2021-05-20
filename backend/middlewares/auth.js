f4fe6bcfec52607a3cf822699abf152d9c60a4c0
Malcolm Xavier <malcolm.x.evans@gmail.com>
2021-05-19 20:19:35 -0400

    test console logs

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,6 +3,7 @@ const UnauthorizedError = require('../middlewares/errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(req.headers);
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError('Authorization Required');
@@ -10,9 +11,11 @@ module.exports = (req, res, next) => {
 
   const token = authorization.replace('Bearer ', '');
   let payload;
+  console.log('payload: ' + payload);
 
   try {
     payload = jwt.verify(token);
+    console.log('payload: ' + payload);
   } catch (err) {
     throw new UnauthorizedError('Authorization Required');
   }

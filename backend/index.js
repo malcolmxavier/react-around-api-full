8dff2d5190a2cdf1f44d2ba7dea70ec77a47548f
Malcolm Xavier <malcolm.x.evans@gmail.com>
2021-05-20 20:32:57 -0400

    remove console logging

--- backend/index.js
+++ backend/index.js
@@ -67,7 +67,6 @@ app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  console.log(err);
   res.status(statusCode).send({ message: statusCode === 500 ? 'An error occurred on the server' : message });
 });
 
